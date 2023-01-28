5e0830558cce7c28d743e45a63b2fb29a69ee10c
unknown <dryldeogracias@gmail.com>
2023-01-28 18:30:01 +0800

    initialized git repository sort

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function openMenu() {
-    document.body.classList += " menu--open"
-  }
-  
-  function closeMenu() {
-    document.body.classList.remove('menu--open')
-  }
+  document.body.classList += " menu--open"
+}
+
+function closeMenu() {
+  document.body.classList.remove('menu--open')
+}
